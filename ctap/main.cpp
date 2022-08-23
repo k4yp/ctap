1815115d6a6a47d17692925a28fad2d70d3b315a
keyp0s <aidentubmann@gmail.com>
2022-08-23 17:07:10 +1000

    fix: easier key rebind

--- ctap/main.cpp
+++ ctap/main.cpp
@@ -1,6 +1,6 @@
 #include "libs.h"
 
-#define scale	   1
+#define scale	   2
 #define SW         640/scale       //screen width
 #define SH         360/scale       //screen height
 #define SW2        (SW/2)          //half of screen width
@@ -21,14 +21,15 @@ struct
 //keys
 struct
 {
-	//move up, down, left, right
+	// player look left, right, up, down
+	int j, k, h, l;
+	
+	//player move forwards, backwards, left, right
 	int w, s, a, d;
+	
+	//player move up, down
+	int z, x;
 
-	//strafe left, right 
-	int sl, sr;
-
-	//move up, down, look up, down
-	int m;
 }K;
 
 //math
@@ -56,66 +57,92 @@ void drawPixel(int x, int y, int r, int g, int b)
 
 void movePlayer()
 {
-	//move up, down
-	if (K.a == 1 && K.m == 0){
+	// player look left, right, up, down
+	if (K.h == 1)
+	{
 		P.a -= 4;
-		if (P.a < 0) 
+		if (P.a < 0)
 		{
-			P.a += 360; 
-		} 
+			P.a += 360;
+		}
 	}
-	if (K.d == 1 && K.m == 0) 
+	if (K.l == 1)
 	{
 		P.a += 4;
-		if (P.a > 359) 
-		{ 
-			P.a -= 360; 
-		} 
+		if (P.a > 359)
+		{
+			P.a -= 360;
+		}
+	}
+	if (K.j == 1)
+	{
+		P.l -= 1;
+	}
+	if (K.k == 1)
+	{
+		P.l += 1;
 	}
 
 	int dx = M.sin[P.a] * 10.0;
 	int dy = M.cos[P.a] * 10.0;
-
-	//move left, right
-	if (K.w == 1 && K.m == 0) 
+	
+	//player move forwards, backwards, left, right
+	if (K.w == 1) 
 	{
 		P.x += dx;
 		P.y += dy; 
 	}
-	if (K.s == 1 && K.m == 0) 
+	if (K.s == 1) 
 	{ 
 		P.x -= dx;
 		P.y -= dy; 
 	}
-
-	//strafe left, right
-	if (K.sr == 1)
+	if (K.a == 1)
+	{
+		P.x -= dy;
+		P.y += dx;
+	}
+	if (K.d == 1)
 	{
 		P.x += dy;
 		P.y -= dx; 
 	}
-	if (K.sl == 1) 
-	{
-		P.x -= dy;
-		P.y += dx; 
-	}
 
-	//move up, down, look up, look down
-	if (K.a == 1 && K.m == 1)
-	{ 
-		P.l -= 1;
-	}
-	if (K.d == 1 && K.m == 1)
+	//player move up, down
+	if (K.z == 1)
 	{
-		P.l += 1; 
-	}
-	if (K.w == 1 && K.m == 1)
-	{
-		P.z -= 4; 
-	}
-	if (K.s == 1 && K.m == 1) 
-	{ 
 		P.z += 4;
+	}
+	if (K.x == 1)
+	{
+		P.z -= 4;
+	}
+}
+
+void drawWall(int x1,int x2, int b1, int b2, int t1, int t2) 
+{
+	int x, y;
+
+	//difference in distance
+	int dyb = b2 - b1;
+	int dyt = t2 - t1;
+	int dx = x2 - x1;
+	if (dx == 0)
+	{
+		dx = 1;
+	}
+	int xs = x1;
+
+	for (x = x1; x < x2; x++) 
+	{
+		int y1 = dyb * (x - xs + 0.5) / dx + b1;
+		int y2 = dyt * (x - xs + 0.5) / dx + t1;
+
+		for (y = y1; y < y2; y++) 
+		{
+			drawPixel(x, y, 255, 0, 0);
+		}
+
 	}
 }
 
@@ -135,13 +162,22 @@ void draw3D()
 	wx[0] = x1 * CS - y1 * SN;
 	wx[1] = x2 * CS - y2 * SN;
 
+	wx[2] = wx[0];
+	wx[3] = wx[1];
+
 	//world y position
 	wy[0] = y1 * CS + x1 * SN;
 	wy[1] = y2 * CS + x2 * SN;
 
+	wy[2] = wy[0];
+	wy[3] = wy[1];
+
 	//world z position
 	wz[0] = 0 - P.z + ((P.l * wy[0]) / 32.0);
 	wz[1] = 0 - P.z + ((P.l * wy[1]) / 32.0);
+
+	wz[2] = wz[0] + 40;
+	wz[3] = wz[1] + 40;
 
 	//screen x, y position
 	wx[0] = wx[0] * 200 / wy[0] + SW2;
@@ -150,18 +186,13 @@ void draw3D()
 	wx[1] = wx[1] * 200 / wy[1] + SW2;
 	wy[1] = wz[1] * 200 / wy[1] + SH2;
 
+	wx[2] = wx[2] * 200 / wy[2] + SW2;
+	wy[2] = wz[2] * 200 / wy[2] + SH2;
 
-	//draw points
-	if (wx[0]>0 && wx[0]<SW && wy[0]>0 && wy[0]<SH)
-	{
-		drawPixel(wx[0], wy[0], 0, 255, 0);
-	}
+	wx[3] = wx[3] * 200 / wy[3] + SW2;
+	wy[3] = wz[3] * 200 / wy[3] + SH2;
 
-	if (wx[1]>0 && wx[1]<SW && wy[1]>0 && wy[1]<SH)
-	{
-		drawPixel(wx[1], wy[1], 255, 0, 0);
-	}
-
+	drawWall(wx[0], wx[1], wy[0], wy[1], wy[2], wy[3]);
 }
 
 void display()
@@ -184,6 +215,22 @@ void display()
 
 void KeysDown(unsigned char key, int x, int y)
 {
+	if (key == 'h' == 1)
+	{
+		K.h = 1;
+	}
+	if (key == 'l' == 1)
+	{
+		K.l = 1;
+	}
+	if (key == 'j' == 1)
+	{
+		K.j = 1;
+	}
+	if (key == 'k' == 1)
+	{
+		K.k = 1;
+	}
 	if (key == 'w' == 1)
 	{
 		K.w = 1;
@@ -200,22 +247,34 @@ void KeysDown(unsigned char key, int x, int y)
 	{
 		K.d = 1;
 	}
-	if (key == 'm' == 1)
+	if (key == 'z' == 1)
 	{
-		K.m = 1;
+		K.z = 1;
 	}
-	if (key == ',' == 1)
+	if (key == 'x' == 1)
 	{
-		K.sr = 1;
-	}
-	if (key == '.' == 1)
-	{
-		K.sl = 1;
+		K.x = 1;
 	}
 }
 
 void KeysUp(unsigned char key, int x, int y)
 {
+	if (key == 'h' == 1)
+	{
+		K.h = 0;
+	}
+	if (key == 'l' == 1)
+	{
+		K.l = 0;
+	}
+	if (key == 'j' == 1)
+	{
+		K.j = 0;
+	}
+	if (key == 'k' == 1)
+	{
+		K.k = 0;
+	}
 	if (key == 'w' == 1)
 	{
 		K.w = 0;
@@ -232,17 +291,13 @@ void KeysUp(unsigned char key, int x, int y)
 	{
 		K.d = 0;
 	}
-	if (key == 'm' == 1)
+	if (key == 'z' == 1)
 	{
-		K.m = 0;
+		K.z = 0;
 	}
-	if (key == ',' == 1)
+	if (key == 'x' == 1)
 	{
-		K.sr = 0;
-	}
-	if (key == '.' == 1)
-	{
-		K.sl = 0;
+		K.x = 0;
 	}
 }
 
@@ -253,7 +308,7 @@ void init()
 		M.sin[i] = sin(i / 180.0 * PI);
 	}
 
-	P.x = 70;
+	P.x = 80;
 	P.y = -110;
 	P.z = 20;
 	P.a = 0;
